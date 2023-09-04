a2b7fa75853b80b07bd9a64eea7d6539ca4deda3
Ger O'Mahony <gomahony@kitmanlabs.com>
2023-09-04 07:53:14 +0100

    Add git signs to the gutter

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,4 +5,4 @@ vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decr
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.o.timeoutlen = 400
-vim.o.statuscolumn = "%l %r"
+vim.o.statuscolumn = "%l %r %s"
