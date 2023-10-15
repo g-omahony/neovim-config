05d926acc8b7a6022267f7347a5223d209309023
Ger O'Mahony <gomahony@kitmanlabs.com>
2023-10-15 15:16:59 +0100

    Upgrade Lazy to 10.2

--- lua/plugins/rubocop.lua
+++ lua/plugins/rubocop.lua
@@ -1,7 +1,13 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
+  "nvimtools/none-ls.nvim",
+  event = "LazyFile",
+  dependencies = { "mason.nvim" },
   opts = function(_, opts)
     local nls = require("null-ls")
-    opts.sources = vim.list_extend(opts.sources, { nls.builtins.formatting.rubocop, nls.builtins.diagnostics.rubocop })
+    opts.root_dir = opts.root_dir or require("null-ls.utils").root_pattern("Gemfile", ".git")
+    opts.sources = vim.list_extend(opts.sources or {}, {
+      nls.builtins.formatting.rubocop,
+      nls.builtins.diagnostics.rubocop,
+    })
   end,
 }
